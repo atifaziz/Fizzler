7d626288600208a23a02df853dd6e1583126f6e8
Colin Ramsay <colinramsay@gmail.com>
2008-10-05 02:12:07 +0000

    comma support, ie:
    a.blog, p
    a.blog , p
    a.blog ,p
    a.blog,p

--- Fizzler.Parser/SelectorEngine.cs
+++ Fizzler.Parser/SelectorEngine.cs
@@ -23,76 +23,68 @@ namespace Fizzle.Parser
 			return document.DocumentNode;
 		}
 
-		public IList<HtmlNode> Parse(string selector)
+		public IList<HtmlNode> Parse(string selectorChain)
 		{
 			HtmlNode documentNode = GetDocumentNode();
+			List<HtmlNode> data = new List<HtmlNode>();
 
-			// we also need to check if a chunk contains a "." character....
-			var chunks = selector.Split(" >".ToCharArray());
-			//var chunks = new List<string>();
-			//foreach (var chunk in initialChunks)
-			//{
-			//    // check if there is a dot at any position other than the first character, aka index zero.
-			//    if(chunk.Trim().LastIndexOf(".") > 0)
-			//    {
-			//        var miniChunks = chunk.Split(".".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
-
-			//        foreach (var miniChunk in miniChunks)
-			//        {
-			//            chunks.Add("." + miniChunk);
-			//        }	
-			//    }
-			//    else
-			//    {
-			//        chunks.Add(chunk);
-			//    }
-			//}
-
-			List<HtmlNode> list = documentNode.ChildNodes.ToList();
-
-			for (int i1 = 0; i1 < chunks.Length; i1++)
+			var selectors = selectorChain.Split(",".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+			
+			foreach (string rawSelector in selectors)
 			{
-				var chunk = chunks[i1];
-				var previousChunk = i1 > 0 ? chunks[i1 - 1] : null;
+				string selector = rawSelector.Trim();
+			
+				// we also need to check if a chunk contains a "." character....
+				var chunks = selector.Split(" >".ToCharArray());
 
-				list = list.Flatten();
-				IList<HtmlNode> remove = new List<HtmlNode>();
-				IList<HtmlNode> keep = new List<HtmlNode>();
+				List<HtmlNode> list = documentNode.ChildNodes.ToList();
 
-				foreach (var node in list)
+				for (int i1 = 0; i1 < chunks.Length; i1++)
 				{
-					if (!_nodeMatcher.IsMatch(node, chunk, previousChunk))
-					{
-						remove.Add(node);
-					}
-					else
-					{
-						keep.Add(node);
-					}
-				}
+					var chunk = chunks[i1];
+					var previousChunk = i1 > 0 ? chunks[i1 - 1] : null;
 
-				for (int i = 0; i < remove.Count; i++)
-				{
-					var node = remove[i];
+					list = list.Flatten();
+					IList<HtmlNode> remove = new List<HtmlNode>();
+					IList<HtmlNode> keep = new List<HtmlNode>();
 
-					foreach (var htmlNode in list)
+					foreach (var node in list)
 					{
-						if (htmlNode.ParentNode == node && !keep.Contains(htmlNode) && !remove.Contains(htmlNode))
+						if (!_nodeMatcher.IsMatch(node, chunk, previousChunk))
 						{
-							remove.Add(htmlNode);
+							remove.Add(node);
+						}
+						else
+						{
+							keep.Add(node);
 						}
 					}
+
+					for (int i = 0; i < remove.Count; i++)
+					{
+						var node = remove[i];
+
+						foreach (var htmlNode in list)
+						{
+							if (htmlNode.ParentNode == node && !keep.Contains(htmlNode) && !remove.Contains(htmlNode))
+							{
+								remove.Add(htmlNode);
+							}
+						}
+					}
+
+					foreach (var node in remove)
+					{
+						list.Remove(node);
+					}
+
+					remove.Clear();
 				}
 
-				foreach (var node in remove)
-				{
-					list.Remove(node);
-				}
-
-				remove.Clear();
+				data.AddRange(list);
 			}
 
-			return list;
+			return data;
 		}
 	}
 }
--- Fizzler.Tests/MultipleSelectors.cs
+++ Fizzler.Tests/MultipleSelectors.cs
@@ -5,5 +5,36 @@ namespace Fizzler.Tests
 	[TestClass]
 	public class MultipleSelectors : SelectorBaseTest
 	{
+		[TestMethod]
+		public void CommaSupport_With_No_Space()
+		{
+			var result = Parser.Parse("p.hiclass,a");
+
+			Assert.AreEqual(2, result.Count);
+		}
+
+		[TestMethod]
+		public void CommaSupport_With_Post_Pended_Space()
+		{
+			var result = Parser.Parse("p.hiclass, a");
+			
+			Assert.AreEqual(2, result.Count);
+		}
+
+		[TestMethod]
+		public void CommaSupport_With_Pre_Post_Pended_Space()
+		{
+			var result = Parser.Parse("p.hiclass , a");
+
+			Assert.AreEqual(2, result.Count);
+		}
+
+		[TestMethod]
+		public void CommaSupport_With_Pre_Pended_Space()
+		{
+			var result = Parser.Parse("p.hiclass ,a");
+
+			Assert.AreEqual(2, result.Count);
+		}
 	}
 }
