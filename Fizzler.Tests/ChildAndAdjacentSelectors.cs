7042dcbf8076b706211a2aa0510bcd38bec59be9
Colin Ramsay <colinramsay@gmail.com>
2008-10-09 21:30:08 +0000

    amend incorrect adjacent test

--- Fizzler.Tests/ChildAndAdjacentSelectors.cs
+++ Fizzler.Tests/ChildAndAdjacentSelectors.cs
@@ -53,31 +53,31 @@ namespace Fizzler.Tests
 		[TestMethod]
 		public void Adjacent_With_Pre_And_Post_Space()
 		{
-			Assert.AreEqual(2, Parser.Parse("a + span").Count, "Adjacent not supported yet.");
+			Assert.AreEqual(1, Parser.Parse("a + span").Count, "Adjacent not supported yet.");
 		}
 
 		[TestMethod]
 		public void Adjacent_With_Post_Space()
 		{
-			Assert.AreEqual(2, Parser.Parse("a+ span").Count, "Adjacent not supported yet.");
+			Assert.AreEqual(1, Parser.Parse("a+ span").Count, "Adjacent not supported yet.");
 		}
 
 		[TestMethod]
 		public void Adjacent_With_Pre_Space()
 		{
-			Assert.AreEqual(2, Parser.Parse("a +span").Count, "Adjacent not supported yet.");
+			Assert.AreEqual(1, Parser.Parse("a +span").Count, "Adjacent not supported yet.");
 		}
 
 		[TestMethod]
 		public void Adjacent_With_No_Space()
 		{
-			Assert.AreEqual(2, Parser.Parse("a+span").Count, "Adjacent not supported yet.");
+			Assert.AreEqual(1, Parser.Parse("a+span").Count, "Adjacent not supported yet.");
 		}
 
 		[TestMethod]
 		public void Comma_Child_And_Adjacent()
 		{
-			Assert.AreEqual(4, Parser.Parse("a + span, div > p").Count, "Adjacent not supported yet.");
+			Assert.AreEqual(3, Parser.Parse("a + span, div > p").Count, "Adjacent not supported yet.");
 		}
 	}	
 }
