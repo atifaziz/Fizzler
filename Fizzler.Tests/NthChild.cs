483c309027af0743e76204f5da475783ae42e7d2
Colin Ramsay <colinramsay@gmail.com>
2008-10-09 22:19:20 +0000

    add basic nth-child tests

--- Fizzler.Tests/NthChild.cs
+++ Fizzler.Tests/NthChild.cs
@@ -1,7 +1,37 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 namespace Fizzler.Tests
 {
-	public class NthChild
+	[TestClass]
+	public class NthChild : SelectorBaseTest
 	{
-		
+		[TestMethod]
+		public void No_Prefix_With_Digit()
+		{
+			var result = Parser.Parse("*:nth-child(2)");
+
+			Assert.AreEqual(2, result.Count);
+			Assert.AreEqual("p", result[0].Name);
+			Assert.AreEqual("span", result[1].Name);
+		}
+	
+		[TestMethod]
+		public void Star_Prefix_With_Digit()
+		{
+			var result = Parser.Parse("*:nth-child(2)");
+
+			Assert.AreEqual(2, result.Count);
+			Assert.AreEqual("p", result[0].Name);
+			Assert.AreEqual("span", result[1].Name);
+		}
+
+		[TestMethod]
+		public void Element_Prefix_With_Digit()
+		{
+			var result = Parser.Parse("p:nth-child(2)");
+
+			Assert.AreEqual(1, result.Count);
+			Assert.AreEqual("span", result[1].Name);
+		}
 	}
 }
