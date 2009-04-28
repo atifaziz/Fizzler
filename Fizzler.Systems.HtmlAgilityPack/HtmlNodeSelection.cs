a1761a7e4c8b80828293c862417a6354cea2cf0b
Atif Aziz <code@raboof.com>
2009-04-28 00:45:09 +0000

    Added files that belong to r115.

---
+++ Fizzler.Systems.HtmlAgilityPack/HtmlNodeSelection.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HtmlAgilityPack;
+
+namespace Fizzler.Systems.HtmlAgilityPack
+{
+    /// <summary>
+    /// Selector API for <see cref="HtmlNode"/>.
+    /// </summary>
+    /// <remarks>
+    /// For more information, see <a href="http://www.w3.org/TR/selectors-api/">Selectors API</a>.
+    /// </remarks>
+    public static class HtmlNodeSelection
+    {
+        /// <summary>
+        /// Similar to <see cref="QuerySelectorAll" /> except it returns 
+        /// only the first element matching the supplied selector strings.
+        /// </summary>
+        public static HtmlNode QuerySelector(this HtmlNode node, string selector)
+        {
+            return node.QuerySelectorAll(selector).FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Retrieves all element nodes from descendants of the starting 
+        /// element node that match any selector within the supplied 
+        /// selector strings. 
+        /// </summary>
+        public static IEnumerable<HtmlNode> QuerySelectorAll(this HtmlNode node, string selector)
+        {
+            var generator = new SelectorGenerator<HtmlNode>(new HtmlNodeOps());
+            Parser.Parse(selector, generator);
+            return generator.Selector(Enumerable.Repeat(node, 1));
+        }
+    }
+}
---
+++ Fizzler.Systems.WinForms/ControlSelection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Fizzler.Systems.WinForms
+{
+    /// <summary>
+    /// Selectors API for <see cref="Control"/>.
+    /// </summary>
+    /// <remarks>
+    /// For more information, see <a href="http://www.w3.org/TR/selectors-api/">Selectors API</a>.
+    /// </remarks>
+    public static class ControlSelection
+    {
+        /// <summary>
+        /// Similar to <see cref="QuerySelectorAll" /> except it returns 
+        /// only the first control matching the supplied selector strings.
+        /// </summary>
+        public static Control QuerySelector(this Control control, string selector)
+        {
+            return Enumerable.FirstOrDefault(control.QuerySelectorAll(selector));
+        }
+
+        /// <summary>
+        /// Retrieves all controls from descendants of the starting control 
+        /// that match any selector within the supplied selector strings. 
+        /// </summary>
+        public static IEnumerable<Control> QuerySelectorAll(this Control control, string selector)
+        {
+            var generator = new SelectorGenerator<Control>(new ControlOps());
+            Parser.Parse(selector, generator);
+            return generator.Selector(Enumerable.Repeat(control, 1));
+        }
+    }
+}
