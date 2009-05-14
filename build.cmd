fb3f7ed5b0791122661892c58f18e548ab127b76
Atif Aziz <code@raboof.com>
2009-05-14 23:30:58 +0000

    Updated build batch script to all additional argument to MSBuild, like target.

--- build.cmd
+++ build.cmd
@@ -1,4 +1,4 @@
 @echo off
 for %%i in (Debug Release) do (
-    "%SystemRoot%\Microsoft.NET\Framework\v3.5\msbuild" /p:Configuration=%%i "%~dp0Fizzler.sln"
+    "%SystemRoot%\Microsoft.NET\Framework\v3.5\msbuild" /p:Configuration=%%i "%~dp0Fizzler.sln" %*
 )
