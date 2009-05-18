d0dfed23b5139400eeaa2efb954cb9a2f1c51603
Atif Aziz <code@raboof.com>
2009-05-18 11:03:54 +0000

    Updated Reader to be public so it can be tested and also added enumeration support to generalize the implementation further.

--- Fizzler/Reader.cs
+++ Fizzler/Reader.cs
@@ -1,6 +1,6 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
 
 namespace Fizzler
 {
@@ -8,21 +8,39 @@ namespace Fizzler
     /// Adds reading semantics to a base <see cref="IEnumerator{T}"/> with the 
     /// option to un-read and insert new elements while consuming the source.
     /// </summary>
-
-    internal sealed class Reader<T> : IDisposable
+    public sealed class Reader<T> : IDisposable, IEnumerable<T>
     {
         private IEnumerator<T> _enumerator;
         private Stack<T> _buffer;
 
+        /// <summary>
+        /// Initialize a new <see cref="Reader{T}"/> with a base
+        /// <see cref="IEnumerable{T}"/> object.
+        /// </summary>
+        public Reader(IEnumerable<T> e) :
+            this(CheckNonNull(e).GetEnumerator()) { }
+
+        private static IEnumerable<T> CheckNonNull(IEnumerable<T> e)
+        {
+            if (e == null) throw new ArgumentNullException("e");
+            return e;
+        }
+
+        /// <summary>
+        /// Initialize a new <see cref="Reader{T}"/> with a base
+        /// <see cref="IEnumerator{T}"/> object.
+        /// </summary>
         public Reader(IEnumerator<T> e)
         {
-            Debug.Assert(e != null);
-
+            if(e == null) throw new ArgumentNullException("e");
             _enumerator = e;
             _buffer = new Stack<T>();
             RealRead();
         }
 
+        /// <summary>
+        /// Indicates whether there is, at least, one value waiting to be read or not.
+        /// </summary>
         public bool HasMore
         {
             get
@@ -32,12 +50,18 @@ namespace Fizzler
             }
         }
 
+        /// <summary>
+        /// Pushes back a new value that will be returned on the next read.
+        /// </summary>
         public void Unread(T value)
         {
             EnsureAlive();
             _buffer.Push(value);
         }
 
+        /// <summary>
+        /// Reads and returns the next value.
+        /// </summary>
         public T Read()
         {
             if (!HasMore)
@@ -51,6 +75,12 @@ namespace Fizzler
             return value;
         }
 
+        /// <summary>
+        /// Peeks the next value waiting to be read.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">
+        /// Thrown if there is no value waiting to be read.
+        /// </exception>
         public T Peek()
         {
             if (!HasMore)
@@ -59,15 +89,25 @@ namespace Fizzler
             return _buffer.Peek();
         }
 
-        public void BulkReadTo(ICollection<T> collection)
+        IEnumerator IEnumerable.GetEnumerator()
         {
-            if (collection == null)
-                throw new ArgumentNullException("collection");
+            return GetEnumerator();
+        }
 
+        /// <summary>
+        /// Returns an enumerator that iterates through the remaining
+        /// values to be read.
+        /// </summary>
+        public IEnumerator<T> GetEnumerator()
+        {
             EnsureAlive();
+            return GetEnumeratorImpl();
+        }
 
-            while (!HasMore)
-                collection.Add(Read());
+        private IEnumerator<T> GetEnumeratorImpl()
+        {
+            while (HasMore)
+                yield return Read();
         }
 
         private void RealRead()
@@ -78,12 +118,19 @@ namespace Fizzler
                 Unread(_enumerator.Current);
         }
 
+        /// <summary>
+        /// Disposes the enumerator used to initialize this object
+        /// if that enumerator supports <see cref="IDisposable"/>.
+        /// </summary>
         public void Close()
         {
             Dispose();
         }
 
-        void IDisposable.Dispose() { Dispose(); }
+        void IDisposable.Dispose()
+        {
+            Dispose();
+        }
 
         void Dispose()
         {
