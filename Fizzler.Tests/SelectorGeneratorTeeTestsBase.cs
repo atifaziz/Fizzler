d6037786e5f9c90f7326fc82e8a9298c80f6a720
Atif Aziz <code@raboof.com>
2009-05-18 09:44:11 +0000

    Updated SelectorGeneratorTee tests to also check for the right order of call (primary then secondary).

--- Fizzler.Tests/SelectorGeneratorTeeTestsBase.cs
+++ Fizzler.Tests/SelectorGeneratorTeeTestsBase.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
@@ -33,21 +34,52 @@ namespace Fizzler.Tests
 
             var args = (from ConstantExpression a in methodCall.Arguments select a.Value).ToArray();
 
-		    var method = methodCall.Method;
+		    var calls = new Queue<Call>(2);
+		    Primary.OnCall = calls.Enqueue;
+            Secondary.OnCall = calls.Enqueue;
+
+		    var method = MapMethod<ISelectorGenerator>(methodCall.Method);
 		    method.Invoke(Tee, args);
 
-			// Assert the fact that the pri and sec methods were both called
-			Assert.AreEqual(method.Name, Primary.LastMethod);
-			Assert.AreEqual(method.Name, Secondary.LastMethod);
+            // Assert the fact that the primary and secondary methods were 
+            // both called with the same arguments and in the right order!
+            
+            var call = calls.Dequeue();
+            Assert.AreSame(Primary, call.Target);
+            Assert.AreSame(method, MapMethod<ISelectorGenerator>(call.Method));
+            Assert.AreEqual(args, call.Arguments);
 
-			Assert.AreEqual(args, Primary.LastArgs);
-			Assert.AreEqual(args, Secondary.LastArgs);
+            call = calls.Dequeue();
+            Assert.AreSame(Secondary, call.Target);
+            Assert.AreSame(method, MapMethod<ISelectorGenerator>(call.Method));
+            Assert.AreEqual(args, call.Arguments);
 		}
 
-		protected class FakeSelectorGenerator : ISelectorGenerator
+        private static MethodInfo MapMethod<T>(MethodInfo method) where T : class
+        {
+            var mapping = method.ReflectedType.GetInterfaceMap(typeof(T));
+            return mapping.InterfaceMethods
+                          .Select((m, i) => new { Source = m, Target = mapping.TargetMethods[i] })
+                          .Single(m => m.Target == method).Source;
+        }
+
+	    protected sealed class Call
+        {
+            public object Target { get; private set; }
+            public MethodInfo Method { get; private set; }
+            public object[] Arguments { get; private set; }
+
+            public Call(object target, MethodInfo method, object[] arguments)
+            {
+                Target = target;
+                Method = method;
+                Arguments = arguments;
+            }
+        }
+
+	    protected class FakeSelectorGenerator : ISelectorGenerator
 		{
-		    public string LastMethod { get; private set; }
-		    public object[] LastArgs { get; private set; }
+	        public Action<Call> OnCall;
 
 		    public void OnInit()
 			{
@@ -166,8 +198,7 @@ namespace Fizzler.Tests
 			
 			private void OnInvoked(MethodBase method, params object[] args)
 			{
-			    LastMethod = method.Name;
-				LastArgs = args;
+                OnCall(new Call(this, (MethodInfo)method, args));
 			}
 		}
 	}
