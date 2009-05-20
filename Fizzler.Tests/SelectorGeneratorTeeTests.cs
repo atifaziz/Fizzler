5cf53ca1917e24ceaf52fcd8e834f1a2fb29f47f
Colin Ramsay <colinramsay@gmail.com>
2009-05-20 09:18:25 +0000

    .net 2.0 build using nant, add nant to repository

--- Fizzler.Tests/SelectorGeneratorTeeTests.cs
+++ Fizzler.Tests/SelectorGeneratorTeeTests.cs
@@ -1,12 +1,16 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Reflection;
 using NUnit.Framework;
 
 namespace Fizzler.Tests
 {
+	public delegate void FzAction();
+	public delegate void FzActionS2(string s, string s2);
+	public delegate void FzActionS1(string s);
+	public delegate void FzActionI2(int i, int i2);
+
 	/// <summary>
 	/// Ensure that all actions on SelectorGeneratorTee are passed
 	/// to the internal Primary and Secondary SelectorGenerators.
@@ -14,6 +18,18 @@ namespace Fizzler.Tests
 	[TestFixture]
 	public class SelectorGeneratorTeeTests
 	{
+		private static SelectorGeneratorTee tee;
+		private static FakeSelectorGenerator primary;
+		private static FakeSelectorGenerator secondary;
+
+		[SetUp]
+		public void Setup()
+		{
+			primary = new FakeSelectorGenerator();
+			secondary = new FakeSelectorGenerator();
+			tee = new SelectorGeneratorTee(primary, secondary);
+		}
+
 		[Test, ExpectedException(typeof(ArgumentNullException))]
 		public void NullPrimary()
 		{
@@ -33,172 +49,184 @@ namespace Fizzler.Tests
         [Test]
 		public void OnInitTest()
 		{
-			Run(tee => tee.OnInit());
+			Run(tee.OnInit);
 		}
 
 		[Test]
 		public void OnCloseTest()
 		{
-			Run(tee => tee.OnClose());
+			Run(tee.OnClose);
 		}
 
 		[Test]
 		public void OnSelectorTest()
 		{
-			Run(tee => tee.OnSelector());
+			Run(tee.OnSelector);
 		}
 
 		[Test]
 		public void TypeTest()
 		{
-			Run(tee => tee.Type("go"));
+			Run(tee.Type, "go");
 		}
 
 		[Test]
 		public void UniversalTest()
 		{
-			Run(tee => tee.Universal());
+			Run(tee.Universal);
 		}
 
 		[Test]
 		public void IdTest()
 		{
-			Run(tee => tee.Id("hello"));
+			Run(tee.Id, "hello");
 		}
 
 		[Test]
 		public void ClassTest()
 		{
-			Run(tee => tee.Class("hello"));
+			Run(tee.Class, "hello");
 		}
 
 		[Test]
 		public void AttrExistsTest()
 		{
-			Run(tee => tee.AttributeExists("hello"));
+			Run(tee.AttributeExists, "hello");
 		}
 
 		[Test]
 		public void AttExactTest()
 		{
-			Run(tee => tee.AttributeExact("hello", "there"));
+			Run(tee.AttributeExact, "hello", "there");
 		}
 
 		[Test]
 		public void AttrIncludesTest()
 		{
-			Run(tee => tee.AttributeIncludes("hello", "there"));
+			Run(tee.AttributeIncludes, "hello", "there");
 		}
 
 		[Test]
 		public void AttrDashMatchTest()
 		{
-			Run(tee => tee.AttributeDashMatch("hello", "there"));
+			Run(tee.AttributeDashMatch, "hello", "there");
 		}
 
 		[Test]
 		public void AttrPrefixMatchTest()
 		{
-			Run(tee => tee.AttributePrefixMatch("hello", "there"));
+			Run(tee.AttributePrefixMatch,"hello", "there");
 		}
 
 		[Test]
 		public void AttrSuffixMatchTest()
 		{
-			Run(tee => tee.AttributeSuffixMatch("hello", "there"));
+			Run(tee.AttributeSuffixMatch, "hello", "there");
 		}
 
 		[Test]
 		public void AttrSubstringTest()
 		{
-			Run(tee => tee.AttributeSubstring("hello", "there"));
+			Run(tee.AttributeSubstring, "hello", "there");
 		}
 
 		[Test]
 		public void FirstChildTest()
 		{
-			Run(tee => tee.FirstChild());
+			Run(tee.FirstChild);
 		}
 
 		[Test]
 		public void LastChildTest()
 		{
-			Run(tee => tee.LastChild());
+			Run(tee.LastChild);
 		}
 
 		[Test]
 		public void NthChildTest()
 		{
-			Run(tee => tee.NthChild(1,2));
+			Run(tee.NthChild, 1, 2);
 		}
 
 		[Test]
 		public void OnlyChildTest()
 		{
-			Run(tee => tee.OnlyChild());
+			Run(tee.OnlyChild);
 		}
 
 		[Test]
 		public void EmptyTest()
 		{
-			Run(tee => tee.Empty());
+			Run(tee.Empty);
 		}
 
 		[Test]
 		public void ChildTest()
 		{
-			Run(tee => tee.Child());
+			Run(tee.Child);
 		}
 
 		[Test]
 		public void DescendantTest()
 		{
-			Run(tee => tee.Descendant());
+			Run(tee.Descendant);
 		}
 
 		[Test]
 		public void AdjacentTest()
 		{
-			Run(tee => tee.Adjacent());
+			Run(tee.Adjacent);
 		}
 
 		[Test]
 		public void GeneralSiblingTest()
 		{
-			Run(tee => tee.GeneralSibling());
+			Run(tee.GeneralSibling);
 		}
 
-	    /// <summary>
+		private static void Run(FzAction action, params object[] args)
+		{
+			Run(action.Method, args);
+		}
+
+		private static void Run(FzActionS1 action, params object[] args)
+		{
+			Run(action.Method, args);
+		}
+
+		private static void Run(FzActionS2 action, params object[] args)
+		{
+			Run(action.Method, args);
+		}
+
+		private static void Run(FzActionI2 action, params object[] args)
+		{
+			Run(action.Method, args);
+		}
+
+		/// <summary>
 	    /// Take the passed action, run it, and then check that the last method
 	    /// and last args are the same for pri and sec.
 	    /// </summary>
-	    private static void Run(Expression<Action<ISelectorGenerator>> action)
+	    private static void Run(MethodBase action, params object[] args)
 	    {
-	        var call = ((MethodCallExpression) action.Body);
-	        var args = (from ConstantExpression arg in call.Arguments select arg.Value).ToArray();
-
-            var primary = new FakeSelectorGenerator();
-            var secondary = new FakeSelectorGenerator();
-            var tee = new SelectorGeneratorTee(primary, secondary);
-            
             var recordings = new Queue<CallRecording<ISelectorGenerator>>(2);
 	        primary.Recorder = recordings.Enqueue;
 	        secondary.Recorder = recordings.Enqueue;
 
-	        var method = call.Method;
-	        method.Invoke(tee, args);
+	        action.Invoke(tee, args);
 
 	        // Assert the fact that the primary and secondary methods were 
 	        // both called with the same arguments and in the right order!
             
 	        var recording = recordings.Dequeue();
 	        Assert.AreSame(primary, recording.Target);
-	        Assert.AreSame(method, MapMethod<ISelectorGenerator>(recording.Method));
+			Assert.AreEqual(action.Name, MapMethod<ISelectorGenerator>(recording.Method).Name);
 	        Assert.AreEqual(args, recording.Arguments);
 
 	        recording = recordings.Dequeue();
 	        Assert.AreSame(secondary, recording.Target);
-	        Assert.AreSame(method, MapMethod<ISelectorGenerator>(recording.Method));
+			Assert.AreEqual(action.Name, MapMethod<ISelectorGenerator>(recording.Method).Name);
 	        Assert.AreEqual(args, recording.Arguments);
 	    }
 
