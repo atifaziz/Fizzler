4e0c2fc052be74f1dd9fc841f149bd112b188699
Atif Aziz <code@raboof.com>
2013-01-05 08:46:20 +0100

    Upgrade to VS 2012 and MSBuild 4

--- build.cmd
+++ build.cmd
@@ -1,4 +1,16 @@
 @echo off
-for %%i in (Debug Release) do (
-    "%SystemRoot%\Microsoft.NET\Framework\v3.5\msbuild" /p:Configuration=%%i "%~dp0Fizzler.sln" %*
+setlocal
+pushd "%~dp0"
+call :main %*
+popd
+goto :EOF
+
+:main
+set MSBUILDEXE=%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe
+if not exist "%MSBUILDEXE%" (
+    echo Microsoft .NET Framework 4.0 does not appear to be installed on this 
+    echo machine, which is required to build the solution.
+    exit /b 1
 )
+for %%s in (*.sln) do for %%c in (Debug Release) do if not errorlevel 1 "%MSBUILDEXE%" "%%s" /p:Configuration=%%c /v:m %*
+goto :EOF
