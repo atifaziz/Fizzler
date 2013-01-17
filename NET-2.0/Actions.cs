fbf3bc5319af30861bbeccf2064eb01c68e52a04
Atif Aziz <code@raboof.com>
2013-01-18 00:50:57 +0100

    Deleting unused files

--- NET-2.0/Actions.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace System
-{
-    internal delegate void Action();
-    internal delegate void Action<T1, T2>(T1 arg1, T2 arg2);
-    internal delegate void Action<T1, T2, T3>(T1 arg1, T2 arg2, T3 arg3);
-}
--- NET-2.0/ExtensionAttribute.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace System.Runtime.CompilerServices
-{
-    class ExtensionAttribute :Attribute
-    {
-    }
-}
