aa46bfb3ef32fb090666258b2530f47e20727549
Atif Aziz <code@raboof.com>
2013-01-19 19:12:09 +0100

    Header text for source files

--- Common/SolutionInfo.cs
+++ Common/SolutionInfo.cs
@@ -1,31 +1,27 @@
-#region License, Terms and Conditions
-//
-// Fizzler - CSS Selector Engine for Microsoft .NET Framework and Mono
-// Copyright (c) 2009, 2010 Atif Aziz, Colin Ramsay. All rights reserved.
-//
-// This library is free software; you can redistribute it and/or modify it under
-// the terms of the GNU Lesser General Public License as published by the Free
-// Software Foundation; either version 3 of the License, or (at your option)
+#region Copyright and License
+// 
+// Fizzler - CSS Selector Engine for Microsoft .NET Framework
+// Copyright (c) 2009 Atif Aziz, Colin Ramsay. All rights reserved.
+// 
+// This library is free software; you can redistribute it and/or modify it under 
+// the terms of the GNU Lesser General Public License as published by the Free 
+// Software Foundation; either version 3 of the License, or (at your option) 
 // any later version.
-//
-// This library is distributed in the hope that it will be useful, but WITHOUT
-// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-// FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+// 
+// This library is distributed in the hope that it will be useful, but WITHOUT 
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+// FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more 
 // details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with this library; if not, write to the Free Software Foundation, Inc.,
+// 
+// You should have received a copy of the GNU Lesser General Public License 
+// along with this library; if not, write to the Free Software Foundation, Inc., 
 // 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
-//
+// 
 #endregion
-
-#region Imports
 
 using System.Reflection;
 using CLSCompliantAttribute = System.CLSCompliantAttribute;
 using ComVisible = System.Runtime.InteropServices.ComVisibleAttribute;
-
-#endregion
 
 //
 // General description
