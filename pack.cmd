9f1f0858a2e7407b62d0d3e4e1be6ce3f9824089
Atif Aziz <code@raboof.com>
2013-01-20 18:56:09 +0100

    PUSHD stack leak in packaging batch script

--- pack.cmd
+++ pack.cmd
@@ -1,7 +1,12 @@
 @echo off
 setlocal
-chcp 1252 > nul
 pushd "%~dp0"
+call :main %*
+popd
+goto :EOF
+
+:main
 if not exist dist md dist
 if not %errorlevel%==0 exit /b %errorlevel%
 call build /v:m && for %%i in (*.nuspec) do .nuget\NuGet pack %%i -OutputDirectory dist
+goto :EOF
