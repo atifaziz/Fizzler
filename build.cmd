96de631a8ee89f39a53409b54ff30c96f6d6a30f
Atif Aziz <code@raboof.com>
2017-09-09 12:23:34 +0200

    Upgrade VS solution file to 15 (2017)

--- build.cmd
+++ build.cmd
@@ -6,11 +6,5 @@ popd
 goto :EOF
 
 :main
-set MSBUILDEXE=%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe
-if not exist "%MSBUILDEXE%" (
-    echo Microsoft .NET Framework 4.0 does not appear to be installed on this 
-    echo machine, which is required to build the solution.
-    exit /b 1
-)
-for %%s in (*.sln) do for %%c in (Debug Release) do if not errorlevel 1 "%MSBUILDEXE%" "%%s" /p:Configuration=%%c /v:m %*
+for %%s in (*.sln) do for %%c in (Debug Release) do if not errorlevel 1 call msbuild "%%s" /p:Configuration=%%c /v:m %*
 goto :EOF
