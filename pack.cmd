089ea32b1818216565a24b1f9e90240809ec79fe
Atif Aziz <code@raboof.com>
2017-12-06 16:38:00 +0100

    Setup for CI

--- pack.cmd
+++ pack.cmd
@@ -8,9 +8,14 @@ goto :EOF
 :main
 if not exist dist md dist
 if not %errorlevel%==0 exit /b %errorlevel%
-set dotpack=dotnet pack -o ..\..\dist --include-source --include-symbols
+set dotpack=dotnet pack -o ..\..\dist --include-source --include-symbols --no-restore --no-build
+set nupack=.nuget\NuGet pack -OutputDirectory dist -NoPackageAnalysis
+if not "%~1"=="" (
+    set dotpack=%dotpack% --version-suffix %1
+    set nupack=%nupack% -Properties VersionSuffix=-%1
+)
 call build /v:m ^
   && %dotpack% src\Fizzler ^
   && %dotpack% src\Fizzler.Systems.HtmlAgilityPack ^
-  && .nuget\NuGet pack -OutputDirectory dist -NoPackageAnalysis Fizzler.Tools.nuspec
+  && %nupack% Fizzler.Tools.nuspec
 goto :EOF
